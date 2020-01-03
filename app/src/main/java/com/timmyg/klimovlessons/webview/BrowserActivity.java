1dff54cf2e7598f4a3def85bac7a7b7caf60f50f
GorshkovTimur <gorshkoff.timur@gmail.com>
2020-01-03 16:52:54 +0300

    browser lesson done

---
+++ app/src/main/java/com/timmyg/klimovlessons/webview/BrowserActivity.java
@@ -0,0 +1,50 @@
+package com.timmyg.klimovlessons.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.timmyg.klimovlessons.R;
+
+public class BrowserActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browser);
+
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.loadUrl("https://ya.ru");
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+
+
+    }
+}

2d4e52eabb302bed5e752a862ca733a7c400d524
GorshkovTimur <gorshkoff.timur@gmail.com>
2020-01-12 16:31:25 +0300

    1-st fragment lesson done

---
+++ app/src/main/java/com/timmyg/klimovlessons/fragment/FragmentActivity.java
@@ -0,0 +1,16 @@
+package com.timmyg.klimovlessons.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.timmyg.klimovlessons.R;
+
+public class FragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+}
